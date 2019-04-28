5feaa0922d171d4e5e59730062d1c1bcb5933f96
杨伊博 <527515025@qq.com>
2019-04-28 13:45:37 +0800

    modify db  rules

--- springboot-drools-dynamic-db/src/main/java/com/us/drools/bean/Message.java
+++ springboot-drools-dynamic-db/src/main/java/com/us/drools/bean/Message.java
@@ -10,6 +10,7 @@ public class Message implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private String status;
+    private String content;
 
     public String getStatus() {
         return status;
@@ -17,5 +18,13 @@ public class Message implements Serializable {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
--- springboot-drools-dynamic-db/src/main/java/com/us/drools/service/RulesService.java
+++ springboot-drools-dynamic-db/src/main/java/com/us/drools/service/RulesService.java
@@ -6,6 +6,7 @@ import com.us.drools.bean.Rules;
 import com.us.drools.dao.RulesDao;
 import com.us.drools.util.DroolsUtils;
 import org.drools.compiler.kie.builder.impl.InternalKieModule;
+import org.drools.core.ClassObjectFilter;
 import org.drools.core.rule.FactType;
 import org.kie.api.KieBase;
 import org.kie.api.KieServices;
@@ -26,43 +27,6 @@ import org.springframework.stereotype.Service;
 public class RulesService {
     @Autowired
     private RulesDao rulesDao;
-
-//    //废弃
-//    public String getRules(Integer id) {
-//
-//        String rules = "";
-//        Rules ru = rulesDao.getById(id);
-//        if (ru != null && ru.getRules() != null) {
-//            rules = ru.getRules();
-//        }
-//        String RULESFILE_NAME = "rules.drl";
-//        KieServices kieServices = KieServices.Factory.get();
-//
-//        /**
-//         * 指定kjar包
-//         */
-//        final ReleaseId releaseId = kieServices.newReleaseId("com", "us", "1.0.0");
-//
-//        // 创建初始化的kjar
-//        InternalKieModule kJar = DroolsUtils.createKieJar(kieServices, releaseId,
-//                new ResourceWrapper(ResourceFactory.newByteArrayResource(rules.getBytes()), RULESFILE_NAME));
-//        KieRepository repository = kieServices.getRepository();
-//        repository.addKieModule(kJar);
-//        KieContainer kieContainer = kieServices.newKieContainer(releaseId);
-//        KieSession session = kieContainer.newKieSession();
-//        Message message = new Message();
-//        message.setStatus("0");
-//        //同一个fact第一次不命中
-//        try {
-//            session.insert(message);
-//            session.fireAllRules();
-//        } catch (Exception e) {
-//        } finally {
-//            session.dispose();
-//        }
-//        System.out.println("-----first fire end-------");
-//        return "ok";
-//    }
 
     public String getRulesWrite(Integer id) {
         String rules = "";
@@ -88,7 +52,14 @@ public class RulesService {
         message.setStatus("0");
         ksession.insert(message);
         ksession.fireAllRules();
-        return "ok";
+
+        String result= null;
+        for (Object per : ksession.getObjects(new ClassObjectFilter(Message.class))) {
+            if (((Message) per).getStatus().equals("0")) {
+                result = ((Message) per).getContent();
+            }
+        }
+        return result;
     }
 
 
